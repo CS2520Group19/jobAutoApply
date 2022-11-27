cbf00bf320eb1ed06b10110ff87d7b62ab6ecdae
Eric Chen <eschen@cpp.edu>
2022-11-26 19:47:19 -0800

    added apply to job function (partial)

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 import time 
 import unittest
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 #Open microsoft edge
 browser = webdriver.Edge()
@@ -26,12 +28,31 @@ def filterJobs():
     browser.get("https://www.linkedin.com/jobs/search/")
     WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[aria-label='Easy Apply filter.']")))
     browser.find_element(By.CSS_SELECTOR, "[aria-label='Easy Apply filter.']").click()
+    
+def applyToJob():
+    time.sleep(2)
+    #Find apply button
+    browser.find_element(By.XPATH,"//div[@class='jobs-apply-button--top-card']").click()
+    time.sleep(1)
+    #Select phone number text field
+    browser.find_element(By.XPATH,"//div[@class='display-flex']").click()
+    time.sleep(3)
+    #Type into phone number text field
+    actions = ActionChains(browser)
+    actions.send_keys('111-111-1111')
+    actions.perform()
+    #Click next
+    browser.find_element(By.XPATH,"//button[@aria-label='Continue to next step']").click()
+    
+    
+    
 
 def main():
     #Open linkedin.com
     browser.get('http://www.linkedin.com')
     login()
     filterJobs()
+    applyToJob()
     time.sleep(10)
     browser.quit()
 
