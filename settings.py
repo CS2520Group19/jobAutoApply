0d96dff481f928c2dad8d2a9324400f1ac564961
Samin Hossain <trigasus14@gmail.com>
2022-12-03 23:56:56 -0800

    integrated settings into main

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings:
     # experience: (dict) Contains the skills and the number of years of experience the user has had under those skills.
     # job_query: (str) The string that will be appended to the url link for filterJobs()
     # phone_number: (str) String of the user's phone number
-    def __init__(self, experience = {"python": 1}, job_query = "software engineer", phone_number = "111-111-1111"):
+    def __init__(self, experience = {}, job_query = "software engineer", phone_number = "111-111-1111"):
         self.experience = experience
         self.job_query = job_query.replace(' ', "%20").lower()
         self.phone_number = phone_number
@@ -70,16 +70,16 @@ def fetch_settings():
                     phone_number = settings["phone_number"])
 
 
-'''-----# Usage Examples #-----'''
-# Initialize settings (Only 1 run needed)
-Settings(experience = dict(zip(["java", "c++", "linux", "sql", "python"], [2, 3, 5, 6, 7])), job_query = "software engineer", phone_number = "111-111-1111")
+# '''-----# Usage Examples #-----'''
+# # Initialize settings (Only 1 run needed)
+# Settings(experience = dict(zip(["java", "c++", "linux", "sql", "python"], [2, 3, 5, 6, 7])), job_query = "software engineer", phone_number = "111-111-1111")
 
-# Read in the settings
-settings = fetch_settings()
+# # Read in the settings
+# settings = fetch_settings()
 
-# Set variables
-settings.set_phone_number("323-555-888")
-settings.add_experience("unix", 4)
+# # Set variables
+# settings.set_phone_number("323-555-888")
+# settings.add_experience("unix", 4)
 
-# Get variables
-print(settings.experience, settings.job_query, settings.phone_number)
+# # Get variables
+# print(settings.experience, settings.job_query, settings.phone_number)
